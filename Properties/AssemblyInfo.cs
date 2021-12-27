dde97da329310f03c49a8d30e62316d681d7bd8e
Dirt-e <50072319+Dirt-e@users.noreply.github.com>
2021-12-27 17:05:14 +0100

    Renamed to YAME.exe - Milestone v0.02a

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MOTUS")]
+[assembly: AssemblyTitle("YAME")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MOTUS")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("HexaGo")]
+[assembly: AssemblyProduct("YAME")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
