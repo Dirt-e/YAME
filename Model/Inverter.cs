78fc4b7c30caf60a78838aa45745cb202b2d4e30
Dirt-e <50072319+Dirt-e@users.noreply.github.com>
2022-12-14 14:52:41 +0100

    Added soft inversion for smooth transition (3rd order LP)

--- Model/Inverter.cs
+++ Model/Inverter.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace YAME.Model
 {
@@ -18,6 +19,7 @@ namespace YAME.Model
             set
             {
                 _invertWx = value;
+                lerp_Wx.Run(value);
                 OnPropertyChanged("InvertWx");
             }
         }
@@ -28,6 +30,7 @@ namespace YAME.Model
             set
             {
                 _invertWy = value;
+                lerp_Wy.Run(value);
                 OnPropertyChanged("InvertWy");
             }
         }
@@ -38,6 +41,7 @@ namespace YAME.Model
             set
             {
                 _invertWz = value;
+                lerp_Wz.Run(value);
                 OnPropertyChanged("InvertWz");
             }
         }
@@ -48,6 +52,7 @@ namespace YAME.Model
             set
             {
                 _invertAx = value;
+                lerp_Ax.Run(value);
                 OnPropertyChanged("InvertAx");
             }
         }
@@ -58,6 +63,7 @@ namespace YAME.Model
             set
             {
                 _invertAy = value;
+                lerp_Ay.Run(value);
                 OnPropertyChanged("InvertAy");
             }
         }
@@ -68,21 +74,36 @@ namespace YAME.Model
             set
             {
                 _invertAz = value;
+                lerp_Az.Run(value);
                 OnPropertyChanged("InvertAz");
             }
         }
 
+        Lerp lerp_Wx = new Lerp(2000,LerpOverMethod.LowPass3rdOrder);
+        Lerp lerp_Wy = new Lerp(2000,LerpOverMethod.LowPass3rdOrder);
+        Lerp lerp_Wz = new Lerp(2000,LerpOverMethod.LowPass3rdOrder);
+        Lerp lerp_Ax = new Lerp(2000, LerpOverMethod.LowPass3rdOrder);
+        Lerp lerp_Ay = new Lerp(2000, LerpOverMethod.LowPass3rdOrder);
+        Lerp lerp_Az = new Lerp(2000, LerpOverMethod.LowPass3rdOrder);
+
         public void InvertDataAsNeeded(PreprocessorData data)
         {
             Output = data;
-            //And then modify some
-            if (InvertWx) Output.WX *= -1.0f;
-            if (InvertWy) Output.WY *= -1.0f;
-            if (InvertWz) Output.WZ *= -1.0f;
-                                     
-            if (InvertAx) Output.AX *= -1.0f;
-            if (InvertAy) Output.AY *= -1.0f;
-            if (InvertAz) Output.AZ *= -1.0f;
+
+            //And then modify some...
+            lerp_Wx.Update();
+            lerp_Wy.Update();
+            lerp_Wz.Update();
+            lerp_Ax.Update();
+            lerp_Ay.Update();
+            lerp_Az.Update();
+
+            Output.WX *= (float)Utility.map(lerp_Wx.Ratio_external, 0, 1, 1, -1.0);
+            Output.WY *= (float)Utility.map(lerp_Wy.Ratio_external, 0, 1, 1, -1.0);
+            Output.WZ *= (float)Utility.map(lerp_Wz.Ratio_external, 0, 1, 1, -1.0);
+            Output.AX *= (float)Utility.map(lerp_Ax.Ratio_external, 0, 1, 1, -1.0);
+            Output.AY *= (float)Utility.map(lerp_Ay.Ratio_external, 0, 1, 1, -1.0);
+            Output.AZ *= (float)Utility.map(lerp_Az.Ratio_external, 0, 1, 1, -1.0);
         }
     }
 }
